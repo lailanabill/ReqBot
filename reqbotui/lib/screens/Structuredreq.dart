17b42549cfb3dfed53a8b888405f12724a689c96
shahd soliman <shahd2101760@miuegypt.edu.eg>
2024-11-18 00:47:30 +0200

    Structuredreq2

--- reqbotui/lib/screens/Structuredreq.dart
+++ reqbotui/lib/screens/Structuredreq.dart
@@ -84,4 +84,89 @@ class StructuredRequirementsScreen extends StatelessWidget {
                   status: "Pending",
                   statusColor: Colors.red,
                 ),
-     
+                _buildTaskItem(
+                  context,
+                  iconColor: Colors.pink,
+                  title: "SRS",
+                  status: "Completed",
+                  statusColor: Colors.green,
+                ),
+                _buildTaskItem(
+                  context,
+                  iconColor: Colors.green,
+                  title: "UML",
+                  status: "Pending",
+                  statusColor: Colors.red,
+                ),
+                _buildTaskItem(
+                  context,
+                  iconColor: Colors.orange,
+                  title: "Requirements",
+                  status: "Completed",
+                  statusColor: Colors.green,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTaskItem(BuildContext context,
+      {required Color iconColor,
+      required String title,
+      required String status,
+      required Color statusColor}) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 16),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            blurRadius: 10,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          // Task Icon
+          Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              color: iconColor.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Icon(Icons.folder, color: iconColor, size: 28),
+          ),
+          SizedBox(width: 16),
+          // Task Title
+          Expanded(
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+          ),
+          // Task Status
+          Text(
+            status,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: statusColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
