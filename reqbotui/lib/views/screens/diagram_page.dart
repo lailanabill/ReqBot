8564e9be8942b6a31888fbfffff32c9e782ce549
shahdibrahiim <Shahd2103789@miuegypt.edu.eg>
2025-04-02 01:12:24 +0200

    diagrampageedits

---
+++ reqbotui/lib/views/screens/diagram_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class DiagramPage extends StatelessWidget {
+  final String diagramName;
+  
+  const DiagramPage({Key? key, required this.diagramName}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(diagramName)),
+      body: Center(
+        child: Text('Showing $diagramName', style: TextStyle(fontSize: 20)),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 187, 151, 236), // Custom button color
+              padding: EdgeInsets.symmetric(vertical: 16),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            ),
+           
+}
