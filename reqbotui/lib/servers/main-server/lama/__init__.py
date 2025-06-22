a744a067dc011be2811dfb09a5e601e9fa1ae4fd
3abglil <mks.abogalil666@gmail.com>
2025-06-22 08:26:44 +0300

    deleting old backend server and reconfiguration

--- reqbotui/lib/servers/main-server/lama/__init__.py
+++
@@ -1,5 +0,0 @@
-from .SequenceDiagram import SequenceDiagramDriver
-from .classdiagram import ClassDiagramDriver
-from .contextdiagram import ContextDiagramDriver
-from .databaseschema import DbDiagramDriver
-from .usecasediagram import UseCasDiagramDriver
